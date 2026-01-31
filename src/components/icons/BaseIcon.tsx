c83970bfd5784d6c072b3f12fdbc17762539f46e
Luciano Rodríguez <luckidetrenque@gmail.com>
2026-01-31 02:36:45 -0300

    Cambios en BaseIcon

--- src/components/icons/BaseIcon.tsx
+++ src/components/icons/BaseIcon.tsx
@@ -4,11 +4,7 @@ type IconProps = SVGProps<SVGSVGElement>;
 
 export function BaseIcon({ children, ...props }: IconProps) {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
-      {...props}
-    >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" {...props}>
       {children}
     </svg>
   );
